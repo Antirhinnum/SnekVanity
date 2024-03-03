f952a268ef20424aabf77ab59a105cfad85e0acb
Michael Scott <michaelcscott2022@gmail.com>
2024-03-03 10:03:25 -0500

    Add All Clothes and All Textures Dye Items

---
+++ Content/Items/AllClothesDyeItem.cs
@@ -0,0 +1,27 @@
+﻿using SnekVanity.Core;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SnekVanity.Content.Items;
+
+public sealed class AllClothesDyeItem : ModItem, IDyeShirt, IDyeUndershirt, IDyePants, IDyeShoes
+{
+	public override void SetDefaults()
+	{
+		Item.DefaultToAccessory();
+		Item.vanity = true;
+		Item.value = Item.buyPrice(gold: 2);
+	}
+
+	public override void AddRecipes()
+	{
+		CreateRecipe()
+			.AddIngredient<ShirtDyeItem>()
+			.AddIngredient<UndershirtDyeItem>()
+			.AddIngredient<PantsDyeItem>()
+			.AddIngredient<ShoesDyeItem>()
+			.AddTile(TileID.DyeVat)
+			.Register();
+	}
+}
---
+++ Content/Items/AllPlayerTexturesDyeItem.cs
@@ -0,0 +1,28 @@
+﻿using SnekVanity.Core;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SnekVanity.Content.Items;
+
+public sealed class AllPlayerTexturesDyeItem : ModItem, IDyeHeadSkin, IDyeTorsoSkin, IDyeArmSkin, IDyeHandSkin, IDyeLegSkin, IDyeEyeBlink, IDyeShirt, IDyeUndershirt, IDyePants, IDyeShoes, IDyeEyes, IDyeEyeWhites, IDyeHair
+{
+	public override void SetDefaults()
+	{
+		Item.DefaultToAccessory();
+		Item.vanity = true;
+		Item.value = Item.buyPrice(gold: 3, silver: 50);
+	}
+
+	public override void AddRecipes()
+	{
+		CreateRecipe()
+			.AddIngredient<AllClothesDyeItem>()
+			.AddIngredient<AllSkinDyeItem>()
+			.AddIngredient<EyeDyeItem>()
+			.AddIngredient<ScleraDyeItem>()
+			.AddTile(TileID.DyeVat)
+			.SortAfterFirstRecipesOf(ModContent.ItemType<AllClothesDyeItem>())
+			.Register();
+	}
+}
