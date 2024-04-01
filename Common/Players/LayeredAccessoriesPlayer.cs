dada99c39c369a0e3b489422a3f30f27d47cceb2
Michael Scott <michaelcscott2022@gmail.com>
2024-04-01 11:04:05 -0400

    Eye Patch (Layered)

---
+++ Common/Players/LayeredAccessoriesPlayer.cs
@@ -0,0 +1,49 @@
+﻿using SnekVanity.Common.Hooks;
+using SnekVanity.Common.Systems;
+using SnekVanity.Content.Items;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SnekVanity.Common.Players;
+
+public sealed class LayeredAccessoriesPlayer : ModPlayer, IAddEquipSlots
+{
+	public bool wearingLayeredEyePatch;
+	public int cEyePatch;
+
+	public void ResetVisibleAccessories()
+	{
+		wearingLayeredEyePatch = false;
+	}
+
+	public void ClearDyeSlots()
+	{
+		cEyePatch = 0;
+	}
+
+	public void UpdateEquipSlot(Item item)
+	{
+		if (!CrossModSystem.AsymmetricEquips_ItemOnDefaultSide(item, Player))
+		{
+			return;
+		}
+
+		if (item.ModItem is LayeredEyePatchItem)
+		{
+			wearingLayeredEyePatch = true;
+		}
+	}
+
+	public void UpdateDyeSlots(Item armorItem, Item dyeItem)
+	{
+		if (dyeItem.dye == 0 || !CrossModSystem.AsymmetricEquips_ItemOnDefaultSide(armorItem, Player))
+		{
+			return;
+		}
+
+		if (armorItem.ModItem is LayeredEyePatchItem)
+		{
+			cEyePatch = dyeItem.dye;
+		}
+	}
+}
