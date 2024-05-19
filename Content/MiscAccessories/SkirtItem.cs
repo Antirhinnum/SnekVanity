ad574d8a7b48f1027780d0c2903607f774359de3
Michael Scott <michaelcscott2022@gmail.com>
2024-05-19 00:29:43 -0400

    Skirt
    colon three

---
+++ Content/MiscAccessories/SkirtItem.cs
@@ -0,0 +1,13 @@
+﻿using Terraria.ModLoader;
+
+namespace SnekVanity.Content.MiscAccessories;
+
+[AutoloadEquip(EquipType.Waist)]
+public sealed class SkirtItem : ModItem
+{
+	public override void SetDefaults()
+	{
+		Item.DefaultToAccessory();
+		Item.vanity = true;
+	}
+}
