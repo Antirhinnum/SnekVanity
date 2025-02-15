c51441288606905d59076bfbc14ff3333ce2dc93
Michael Scott <michaelcscott2022@gmail.com>
2025-02-15 11:56:21 -0500

    Fix Asymmetric Equipment Hair Dye Bug

--- Content/OtherShadersAsDyes/HairDyePlayer.cs
+++ Content/OtherShadersAsDyes/HairDyePlayer.cs
@@ -10,7 +10,10 @@ public sealed class HairDyePlayer : ModPlayer
 	public override void ModifyDrawInfo(ref PlayerDrawSet drawInfo)
 	{
 		// Vanilla has some strange condition that applies Life Hair Dye by default???
-		if (Player.head == ArmorIDs.Head.FamiliarWig && Player.hairDye == 0)
+		// Revert the hair dye back to undyed if we think this is the case.
+		if (Player.head == ArmorIDs.Head.FamiliarWig
+			&& Player.hairDye == 0
+			&& drawInfo.hairDyePacked == PlayerDrawHelper.PackShader(1, PlayerDrawHelper.ShaderConfiguration.HairShader))
 		{
 			drawInfo.hairDyePacked = PlayerDrawHelper.PackShader(0, PlayerDrawHelper.ShaderConfiguration.HairShader);
 		}
